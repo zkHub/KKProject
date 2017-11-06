16aedea7c01e6116c1223e137959933760ce2d72
zhangke <192938268@qq.com>
2017-11-06 20:35:56 +0800

    11111

--- KKProject/ViewController.m
+++ KKProject/ViewController.m
@@ -10,23 +10,120 @@
 #import <ReactiveObjC/ReactiveObjC.h>
 #import "TestViewController.h"
 #import <objc/runtime.h>
+#import <AVFoundation/AVFoundation.h>
+#import "AudioPlayer.h"
+#import "XFFollowButton.h"
+
+
+#define urlString @"http://mp3.henduoge.com/s/2017-10-10/1507614801.mp3"
+
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *nameText;
 @property (weak, nonatomic) IBOutlet UITextField *passwordText;
 @property (weak, nonatomic) IBOutlet UIButton *loginBtn;
+@property (nonatomic, strong) AVPlayer *player;
+@property(nonatomic,strong)AudioPlayer *audioPlayer;
 
 @end
 
 @implementation ViewController
 
+- (AVPlayer *)player {
+    if (!_player) {
+        _player = [AVPlayer new];
+    }
+    return _player;
+}
 
+
+//观察者回调
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context
+{
+    //注意这里查看的是self.player.status属性
+//    if ([keyPath isEqualToString:@"status"]) {
+        AVPlayerStatus staute = [change[@"new"] integerValue];
+        switch (staute) {
+            case AVPlayerStatusReadyToPlay:
+                NSLog(@"加载成功,可以播放了");
+                [self.player play];
+                break;
+            case AVPlayerStatusFailed:
+                NSLog(@"加载失败");
+                break;
+            case AVPlayerStatusUnknown:
+                NSLog(@"资源找不到");
+                break;
+                
+                
+            default:
+                break;
+        }
+        
+        NSLog(@"change:%@",change);
+//    }
+    
+    
+//    if ([keyPath isEqualToString:@"loadedTimeRanges"]) {
+//
+//        NSArray * timeRanges = self.player.currentItem.loadedTimeRanges;
+//        //本次缓冲的时间范围
+//        CMTimeRange timeRange = [timeRanges.firstObject CMTimeRangeValue];
+//        //缓冲总长度
+//        NSTimeInterval totalLoadTime = CMTimeGetSeconds(timeRange.start) + CMTimeGetSeconds(timeRange.duration);
+//        //音乐的总时间
+//        NSTimeInterval duration = CMTimeGetSeconds(self.player.currentItem.duration);
+//        //计算缓冲百分比例
+//        NSTimeInterval scale = totalLoadTime/duration;
+//
+//    }
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+//    [_audioPlayer setPrepareMusicUrl:urlString];
+
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
     [UtilityHandler showIvarPropertyMethodForClass:[UIAlertAction class]];
+    CGFloat height = 142 + SCREEN_WIDTH/5;
+    UIView *followView = [[UIView alloc]initWithFrame:CGRectMake(0, SCREEN_HEIGHT -64-height, SCREEN_WIDTH, height)];
+    
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(0, 25, SCREEN_WIDTH, 24)];
+    label.text = @"跟进";
+    label.textAlignment = NSTextAlignmentCenter;
+    [followView addSubview:label];
+    for (int i = 0; i < 5; i++) {
+        XFFollowButton *button = [[XFFollowButton alloc]initWithFrame:CGRectMake(i*SCREEN_WIDTH/5, 300, SCREEN_WIDTH/5, SCREEN_WIDTH/5)];
+        [followView addSubview:button];
+    }
+
+    UIButton *cancelBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [cancelBtn setTitle:@"取消" forState:(UIControlStateNormal)];
+    [cancelBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    cancelBtn.frame = CGRectMake(0, height-48, SCREEN_WIDTH, 48);
+    [followView addSubview:cancelBtn];
+    
+    [self.view addSubview:followView];
+//    if (self.player.currentItem) {
+//        [self.player.currentItem removeObserver:self forKeyPath:@"status"];
+//    }
+
+    //创建一个item资源
+    AVPlayerItem *item = [AVPlayerItem playerItemWithURL:[NSURL URLWithString:urlString]];
+    [item addObserver:self forKeyPath:@"status" options:(NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld) context:nil];
+    [self.player replaceCurrentItemWithPlayerItem:item];
+    [self.player play];
+//    _audioPlayer = [AudioPlayer sharePlayer];
+
+    
+    NSString *str1 = nil;
+    NSString *str = str1 ?: @"2";
+    NSLog(@"%@",str);
     
     NSString *string = @"翟让";
 
---
+++ KKProject/XFFollowButton.h
@@ -0,0 +1,16 @@
+//
+//  XFFollowButton.h
+//  KKProject
+//
+//  Created by 张柯 on 2017/10/16.
+//  Copyright © 2017年 zhangke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XFFollowButton : UIButton
+
+@property (nonatomic, strong) UIImageView *headerView;
+@property (nonatomic, strong) UILabel *label;
+
+@end
