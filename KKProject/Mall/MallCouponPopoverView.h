e63f4cee8c5b21667dfc496dc6c867050f970e8c
zhangke14 <zhangke14@100tal.com>
2020-06-18 19:29:18 +0800

    al

---
+++ KKProject/Mall/MallCouponPopoverView.h
@@ -0,0 +1,17 @@
+//
+//  MallCouponUsingPopoverView.h
+//  KKProject
+//
+//  Created by zk on 2020/5/13.
+//  Copyright © 2020 zhangke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MallCouponUsingPopoverView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
